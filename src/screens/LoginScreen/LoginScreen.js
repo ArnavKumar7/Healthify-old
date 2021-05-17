ea5eae62aba3b15587d05eb3b7072392adcb3f0e
ArnavKumar7 <arnav4776@gmail.com>
2021-05-18 00:59:24 +0530

    updated backend of firebase

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
+import {firebase} from '../../firebase/config'
 
 export default function LoginScreen({navigation}) {
     const [email, setEmail] = useState('')
@@ -12,6 +13,30 @@ export default function LoginScreen({navigation}) {
     }
 
     const onLoginPress = () => {
+         firebase
+             .auth()
+             .signInWithEmailAndPassword(email,password)
+             .then((response) => {
+                 const uid = response.user.uid
+                 const usersRef = firebase.firestore().collection('users')
+                 usersRef
+                     .doc(uid)
+                     .get()
+                     .then(firestoreDocument => {
+                          if(!firestoreDocument.exists) {
+                                alert("User does not exist anymore.")
+                                return;
+                          }
+                          const user = firestoreDocument.data()
+                          navigation.navigate('Home',{user})
+                     })
+                     .catch(error => {
+                           alert(error)
+                     });
+             })
+             .catch(error => {
+                  alert(error)
+             })
     }
 
     return (
@@ -45,7 +70,7 @@ export default function LoginScreen({navigation}) {
                 <TouchableOpacity
                     style={styles.button}
                     onPress={() => onLoginPress()}>
-                    <Text style={styles.buttonTitle}>Log in</Text>
+                    <Text style={styles.buttonTitle}>Login</Text>
                 </TouchableOpacity>
                 <View style={styles.footerView}>
                     <Text style={styles.footerText}>Dont have an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Sign up</Text></Text>
